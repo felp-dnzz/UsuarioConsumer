12be7d2c8689a516923d7508ea2d21268bc23782
Felipe Diniz <felipedinizprogamer@gmail.com>
2026-08-26 14:28:38 -0300

    init: inicializa o projeto

---
+++ src/main/java/org/example/usuarioconsumer/UsuarioConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.example.usuarioconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuarioConsumerApplication.class, args);
+    }
+
+}

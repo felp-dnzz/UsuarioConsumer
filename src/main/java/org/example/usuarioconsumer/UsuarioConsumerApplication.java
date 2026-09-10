69555175f7b98b291e5ff7270860dfe3afb68db0
felp-dnzz <felipedinizprogamer@gmail.com>
2026-09-10 17:06:04 -0300

    feat: adiciona o web client

--- src/main/java/org/example/usuarioconsumer/UsuarioConsumerApplication.java
+++ src/main/java/org/example/usuarioconsumer/UsuarioConsumerApplication.java
@@ -2,12 +2,29 @@ package org.example.usuarioconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class UsuarioConsumerApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(UsuarioConsumerApplication.class, args);
+    }
+    @Bean
+    public WebClient.Builder webClientBuilder(){
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://localhost:4000/")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 
 }

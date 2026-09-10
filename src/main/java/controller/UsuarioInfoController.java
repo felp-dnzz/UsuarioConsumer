a298b475153755ff5d0fd6bf1a39e48cdb843790
Felipe_Diniz <felipedinizprogamer@gmail.com>
2026-09-10 16:21:55 -0300

    feat: adiciona o service usuarioinfo controller

---
+++ src/main/java/controller/UsuarioInfoController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import model.UsuarioInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import service.UsuarioInfoService;
+
+@Controller
+@RequestMapping("/usuarioInfo")
+public class UsuarioInfoController {
+    @Autowired
+    private UsuarioInfoService usuarioInfoService;
+
+    @GetMapping("/new")
+    public String newUsuarioInfoString(Model model) {
+        model.addAttribute("usuarioInfo", new UsuarioInfo());
+        return "usuarioinfo/form";
+    }
+
+    @GetMapping("")
+    public String listUsuarioInfoString(Model model) {
+        model.addAttribute("listUsuarioInfo", usuarioInfoService.findAll());
+        return "usuarioInfo/list";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUsuarioInfoString(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("usuarioInfo", usuarioInfoService.findById(id));
+        return "usuarioInfo/form";
+    }
+
+    @PostMapping("/save")
+    public String saveUsuarioInfo(@ModelAttribute("usuarioInfo") UsuarioInfo usuarioInfo) {
+        usuarioInfoService.save(usuarioInfo);
+        return "redirect:/usuarioInfo";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteUsuarioInfo(@PathVariable("id") Integer id) {
+        usuarioInfoService.delete(id);
+        return "redirect:/usuarioInfo";
+    }
+}
